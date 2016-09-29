96cc7232c151bcc2fc7ca1a320b27a07d98df7d1
Juan Molero <juanmolerog@gmail.com>
2016-09-29 08:17:41 -0400

    Nuevas cosas

--- TeensyPromocion/Nombres.h
+++ TeensyPromocion/Nombres.h
@@ -1,60 +1,60 @@
-const char Nombres[58][11] = {
-    "Celene",  //0
-    "Starky",  //1
-    "German",  //2
-    "Yeimar",  //3
-    "Jonathan",  //4
-    "Erenesto",  //5
-    "Jose",  //6
-    "Douglass",  //7
-    "Javier",  //8
-    "Javier",  //9
-    "Jesus",  //10
-    "Jesus",  //11
-    "Joscar",  //12
-    "Leonardo",  //13
-    "Esteban",  //14
-    "Christian",  //15
-    "Angel",  //16
-    "Ender",  //17
-    "Grecia",  //18
-    "Henry",  //19
-    "Juan",  //20
-    "Juan",  //21
-    "Enmanuel",  //22
-    "Enmanuel",  //23
-    "Andrea",  //24
-    "Alberth",  //25
+const char Nombres[58][12] = {
+    "Alberth",  //0
+    "Aldo",  //1
+    "Americo",  //2
+    "Andrea r.",  //3
+    "Andrea",  //4
+    "Angel",  //5
+    "Angelica",  //6
+    "Antonio",  //7
+    "Betania",  //8
+    "Carlos",  //9
+    "Celene",  //10
+    "Christian",  //11
+    "Daniel z",  //12
+    "Daniel",  //13
+    "Daniela m.",  //14
+    "Douglas",  //15
+    "Ender",  //16
+    "Enmanuel g.",  //17
+    "Enmanuel",  //18
+    "Erika",  //19
+    "Ernesto",  //20
+    "Esteban",  //21
+    "Euro",  //22
+    "Franklin s.",  //23
+    "Franklin",  //24
+    "Freddy",  //25
     "Genesis",  //26
-    "Gerson",  //27
-    "Carlos",  //28
-    "Jesus",  //29
-    "Juan",  //30
-    "Nelson",  //31
-    "Americo",  //32
-    "Daniela",  //33
-    "Luis",  //34
-    "Maria",  //35
-    "Maykol",  //36
-    "Rosana",  //37
-    "Franklin",  //38
-    "Freddy",  //39
-    "Pedro",  //40
-    "Euro",  //41
-    "Keyla",  //42
-    "Reyna",  //43
-    "Giann",  //44
-    "Erika",  //45
-    "Aldo",  //46
-    "Jesus",  //47
-    "Antonio",  //48
-    "Andrea",  //49
-    "Daniel",  //50
-    "Yeinny",  //51
-    "Franklin",  //52
-    "Michael",  //53
-    "Angelica",  //54
-    "Betania",  //55
-    "Daniel",  //56
-    "Joel",  //57
+    "German",  //27
+    "Gerson",  //28
+    "Grecia",  //29
+    "Henry",  //30
+    "Javier c.",  //31
+    "Javier",  //32
+    "Jesus c.",  //33
+    "Jesus m.",  //34
+    "Jesus r.",  //35
+    "Jesus",  //36
+    "Joel",  //37
+    "Jonathan",  //38
+    "Joscar",  //39
+    "Jose c.",  //40
+    "Juan c.f.",  //41
+    "Juan f",  //42
+    "Juan m.",  //43
+    "Keyla",  //44
+    "Leonardo",  //45
+    "Luis m.",  //46
+    "Maikol",  //47
+    "Maria m",  //48
+    "Michael",  //49
+    "Nelson",  //50
+    "Pedro",  //51
+    "Pietro",  //52
+    "Reyna",  //53
+    "Roxana",  //54
+    "Starky",  //55
+    "Yeimar",  //56
+    "Yeinny",  //57
 };
--- generadorNombres.py
+++ generadorNombres.py
@@ -1,34 +1,54 @@
 import os, re
 from PIL import Image
 from resizeimage import resizeimage
+from openpyxl import load_workbook
+
+excel = load_workbook("./ArduinoPromocion/nombres.xlsx")['Hoja1']
+nombres_excel = [x.value.lower().strip() for x in excel['A']]
+nombres_faltan = list(nombres_excel)
+
+reg = re.compile(r"([\w\s\.]*)\.jpg")
 
 dir_files = os.listdir("./EspPromocion/assets/originales")
+dir_files = [x for x in dir_files if reg.search(x) is not None]
 
-nombres = [x for x in sorted(dir_files) if re.search(r"\.jpg$",x) is not None]
+nombres_archivos = [x.group(1).lower() for x in [reg.search(y) for y in sorted(dir_files)]]
 
-length = len(nombres)
+for x in nombres_archivos:
+    if x in nombres_faltan:
+        nombres_faltan.pop(nombres_faltan.index(x))
 
-string_file = ""
+print("Los siguientes nombres carecen de imagenes:")
+for x in nombres_faltan:
+    print(x)
+
+length = len(dir_files)
+
+nombres_file = ""
+arduino_file = ""
 index = 0
 size = 250
 
-with open("./TeensyPromocion/Nombres.h","w") as f:
-    string_file += "const char Nombres[{}][11] = {{\n".format(length)
-    for i, x in enumerate(nombres):
-        match = re.search(r"^([\w\s]*)\.jpg$",x)
+with open("./TeensyPromocion/Nombres.h","w") as f, open("./TeensyPromocion/NombresArduino.h","w") as g:
+    nombres_file += "const char Nombres[{}][12] = {{\n".format(length)
+    arduino_file += "const uint8_t matriz_arduino[{}] = {{\n" .format(length)
+    for i, x in enumerate(sorted(dir_files)):
+        match = re.search(r"^([\w\s\.]*)\.jpg$",x)
         if match is not None:
             nombre = match.group(1)
-            nombre = nombre.split(" ")[1]
             nombre = nombre.lower()
             nombre = "".join([nombre[0].upper(),nombre[1:]])
-            string_file += "    \"{}\",  //{}\n".format(nombre,i)
+            nombres_file += "    \"{}\",  //{}\n".format(nombre,i)
+            arduino_file += "    {}, //{}, {}\n".format(nombres_excel.index(nombres_archivos[i]),i,nombre)
             with open(''.join(['./EspPromocion/assets/originales/',x])) as imagen:
                 with Image.open(imagen) as baseimage:
                     thumbnail = resizeimage.resize_thumbnail(baseimage,[size,size])
                     thumbnail.save("".join(["./EspPromocion/assets/fotos/",str(index),".jpg"]),format='JPEG',quality=30)
                     index += 1
 
-    string_file += "};"
-    f.write(string_file)
+    nombres_file += "};"
+    arduino_file += "};"
+    f.write(nombres_file)
+    g.write(arduino_file)
 
 
